228b814f8367596704aeb2682a87422356f003cc
fmoreirati <fabianomoreira.ti@gmail.com>
2019-10-07 20:28:58 -0300

    07102019

--- src/app/pages/list-usuario/list-usuario.page.ts
+++ src/app/pages/list-usuario/list-usuario.page.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { UsuarioService } from 'src/app/services/usuario.service';
-import { Router } from '@angular/router';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-list-usuario',
