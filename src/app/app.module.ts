5df006fdc2ddcfb8a29ce512a534b24259a23f6d
Técnico em Informática <24121742018.1@senacrjedu.local>
2019-10-14 21:41:38 -0300

    14102019

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,9 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { GooglePlus } from '@ionic-native/google-plus/ngx'
+import { GooglePlus } from '@ionic-native/google-plus/ngx';
+import { Geolocation } from '@ionic-native/geolocation/ngx';
+import { Camera } from '@ionic-native/camera/ngx';
 
 //Firebase ----------------------------
 import { AngularFireModule } from '@angular/fire';
@@ -28,7 +30,9 @@ import { AngularFireAuthModule} from '@angular/fire/auth';
     StatusBar,
     SplashScreen,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
-    GooglePlus
+    GooglePlus, 
+    Geolocation,
+    Camera
   ],
   bootstrap: [AppComponent]
 })
